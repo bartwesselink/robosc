1a37ae5a2c14c58d2ed51857ccefa9ccbc9d3384
Bart Wesselink <b.wesselink@recognize.nl>
2022-06-15 13:51:26 +0200

    Fixate build version

--- language/nl.tue.robotsupervisorycontrollerdsl.cifwrapper/build_escet.sh
+++ language/nl.tue.robotsupervisorycontrollerdsl.cifwrapper/build_escet.sh
@@ -2,13 +2,16 @@
 echo "Cloning ESCET from repository..."
 
 REPO="$1"
-BRANCH="$2"
+SHA="$2"
 
 rm -rf escet-repo
 mkdir -p escet-repo
-git clone --depth 1 $REPO escet-repo -b $BRANCH
-
+git clone --depth 1 $REPO escet-repo
 cd escet-repo
+
+git fetch --depth=1 origin $SHA
+git checkout $SHA
+
 ./mvn_escet.sh -DskipTests clean verify
 rm -rf product/
 rm -rf .git
